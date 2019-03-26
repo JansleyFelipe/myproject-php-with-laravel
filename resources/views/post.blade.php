f80e5c512a86e899ece55c9529d51b4faee81673
Jansley Barbosa <jansley.barbosa@amlabs.com.br>
2019-03-26 17:09:23 -0300

    Laravel-Routes

---
+++ resources/views/post.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+    
+<h1>Post Page {{$id}} {{$name}} {{$passwd}}</h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,69 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/contact', function () {
+//     return "Hi about page";
+// });
+
+// Route::get('/post/{id}/{name}', function($id, $name){
+
+//     return "This is post number " . $id . $name;
+// });
+
+// Route::get('admin/posts/example', array('as' => 'admin.home', function(){
+
+//     $url = route('admin.home');
+
+//     return "This url is " . $url;
+
+// }));
+
+// Route::get('/post/{id}', 'PostsController@index');
+
+// Route::resource('posts', 'PostsController');
+
+// Route::get('/contact', 'PostsController@contact');
+
+// Route::get('post/{id}/{name}/{passwd}', 'PostsController@show_post');
+
+
+
+/*
+|--------------------------------------------------------------------------
+| DATABASE Raw SQL Queries
+|--------------------------------------------------------------------------
+*/
+
+// Route::get('/insert', function(){
+
+//     DB::insert('insert into posts(title, content) values(?, ?)', ['PHP with laravel', 'Laravel is the best thing that has happened to PHP']);
+
+// });
+
+// Route::get('/read', function(){
+
+//     $results = DB::select('select * from posts where id=?', [1]);
+
+//     foreach($results as $post){
+
+//     return $post->content;
+
+//     }
+// });
+
+// Route::get('/update', function(){
+
+//     $updated = DB::update('update posts set title="Updated title" where id=?', [1]);
+
+//     return $updated;
+
+// });
+
+// Route::get('/delete', function(){
+
+//     $deleted = DB::delete('delete from posts where id=?', [1]);
+
+//     return $deleted;
+
+// });
